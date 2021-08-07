6eca8934175e0c7bb403c767d3fef26ef159bbcf
yADGithub <2007andredoe@gmail.com>
2021-08-07 13:53:35 -0300

    update no feed

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'André Lucas',
+    site_url: 'https://blog.adgdeveloper.tk',
+    feed_url: 'https://blog.adgdeveloper.tk/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
