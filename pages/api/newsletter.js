65b78bfc923282f6a191df5fb171ced8b561898d
yADGithub <2007andredoe@gmail.com>
2021-08-08 10:12:55 -0300

    newsletter

---
+++ pages/api/newsletter.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+function getRequstParams(email) {
+    const API_KEY = process.env.MAILCHIMP_API_KEY
+    const LIST_ID = process.env.MAILCHIMP_LIST_ID
+
+    const DATACENTER = process.env.MAILCHIMP_API_KEY.split('-')[1]
+
+    const url = `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${LIST_ID}/members`
+
+    const data = {
+        email_address: email,
+        status: 'subscribed'
+    }
+}
+
+export default async (req, res) => {
+    const {email} = req.body
+
+    if (!email || !email.length) {
+        return res.status(400).json({
+            error: 'Forgot to add your email?'
+        })
+    }
+
+    try {
+        const { url, data, headers } = getRequstParams(email)
+        const response = axios.post(url, data, {headers})
+
+        // Success
+        return res.status(201).json({
+            error: null
+        })
+    } catch (e) {
+        return res.status(400).json({
+            error: 'Oops, something went wrong...'
+        })
+    }
+}
---
+++ pages/newsletter.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import axios from "axios"
+
+export default () => {
+    const [email, setEmail] = useState('')
+    const [state, setState] = useState('IDLE')
+    const [errorMessage, setErrorMessage] = useState(null)
+
+    const subscribe = async () => {
+        setState('LOADING')
+        setErrorMessage(null)
+        try {
+            const response = axios.post('/api/newsletter', { email })
+            setState('SUCCESS')
+        } catch (e) {
+            setErrorMessage(e.response.data.error)
+            setState('ERROR')
+        }
+    }
+
+    return (
+        <center>
+            <h2>Agora tenho uma newsletter!</h2>
+            <input
+                type='text'
+                placeholder='Insira o Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <button disabled={state === 'LOADING'} onClick={subscribe}>
+                Inscreva-se
+            </button>
+            {state === 'ERROR' && (
+            <p>{errorMessage}</p>
+            )}
+        {state === 'SUCCESS' && (
+            <p>Success!</p>
+        )}
+    </center>
+    )
+}
