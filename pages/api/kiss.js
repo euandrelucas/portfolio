17677a5d33550bfae0d44fed71b0fb59db0e13f8
yADGithub <2007andredoe@gmail.com>
2021-08-12 13:49:43 -0300

    icons

--- pages/api/kiss.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export default async (req, res) => {
-    const response = await axios.get('https://nekos.life/api/kiss')
-    const image = response.data
-    return res.status(200).json({
-        link: image.url
-    })
-}
