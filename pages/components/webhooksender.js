110e6eac9e284c21632acdbefe486d016b2d9c64
ADG <65923257+andrelucaas@users.noreply.github.com>
2021-10-22 17:50:44 -0300

    Update webhooksender.js

--- pages/components/webhooksender.js
+++ pages/components/webhooksender.js
@@ -1,7 +1,5 @@
 export default () => {
     return (
-        <center>
         <iframe src="https://webhooksender.4dg.repl.co/" height="100%" width="100%">About</iframe>
-        </center>
     )
 }
