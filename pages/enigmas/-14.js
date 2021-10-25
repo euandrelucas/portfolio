c118dac96e042e7fb0ea8bedc322a7562daaea4e
yADGithub <2007andredoe@gmail.com>
2021-10-25 13:05:19 -0300

    teste

---
+++ pages/enigmas/-14.js
@@ -0,0 +1,9 @@
+export default () => {
+    return (
+        <>
+        <p align="center">
+            <h1>Nada aqui (ainda)!</h1>
+        </p>
+        </>
+    )
+}
---
+++ pages/enigmas/index.js
@@ -0,0 +1,9 @@
+export default () => {
+    return (
+        <>
+        <p align="center">
+        <h1>Nothing Here!</h1>
+        </p>
+        </>
+    )
+}
